de41932ab796be19e08122b7cc56ceaf5ac2042b
J. O. Makar <jomakar@gmail.com>
2017-08-22 12:11:45 -0400

    Added base64 decoding

--- crypto/base64.c
+++ crypto/base64.c
@@ -3,7 +3,67 @@
 #include <c_types.h>
 #include <ets_sys.h>
 
+static uint8_t decmap(uint8_t c);
 static uint8_t encmap(uint8_t c);
+
+ICACHE_FLASH_ATTR int b64decode(const uint8_t *enc, size_t elen, uint8_t *dec, size_t dlen) {
+    size_t i, j;
+    uint8_t last;
+    uint32_t v;
+
+    if (elen % 4 != 0) {
+        ERROR(MAIN, "elen % 4 != 0")
+        return -2;
+    }
+
+    for (i=0; i<elen; i++)
+      if (!((65 <= enc[i] && enc[i] <= 90)  ||
+            (97 <= enc[i] && enc[i] <= 122) ||
+            (48 <= enc[i] && enc[i] <= 57)  ||
+            enc[i] == '+' || enc[i] == '/' || enc[i] == '='))
+      {
+          ERROR(MAIN, "bad input")
+          return -2;
+      }
+
+    if (elen/4*3 > dlen) {
+        ERROR(MAIN, "dec overflow")
+        return -1;
+    }
+
+    for (i=0, j=0; i<elen; i+=4) {
+        last = i+4 >= elen;
+
+        if (last && enc[i+2] == '=' && enc[i+3] == '=') {
+            v = (decmap(enc[i  ])<<18) |
+                (decmap(enc[i+1])<<12);
+
+            dec[j++] = (v & 0x00ff0000) >> 16;
+        }
+
+        else if (last && enc[i+3] == '=') {
+            v = (decmap(enc[i  ])<<18) |
+                (decmap(enc[i+1])<<12) |
+                (decmap(enc[i+2])<< 6);
+
+            dec[j++] = (v & 0x00ff0000) >> 16;
+            dec[j++] = (v & 0x0000ff00) >>  8;
+        }
+
+        else {
+            v = (decmap(enc[i  ])<<18) |
+                (decmap(enc[i+1])<<12) |
+                (decmap(enc[i+2])<< 6) |
+                decmap(enc[i+3]);
+
+            dec[j++] = (v & 0x00ff0000) >> 16;
+            dec[j++] = (v & 0x0000ff00) >>  8;
+            dec[j++] =  v & 0x000000ff;
+        }
+  }
+
+  return 0;
+}
 
 ICACHE_FLASH_ATTR int b64_encode(const uint8_t *dec, size_t dlen, uint8_t *enc, size_t elen) {
     size_t i, j;
@@ -41,6 +101,23 @@ ICACHE_FLASH_ATTR int b64_encode(const uint8_t *dec, size_t dlen, uint8_t *enc, 
     }
 
     return j;
+}
+
+ICACHE_FLASH_ATTR uint8_t decmap(uint8_t c) {
+  if (65 <= c && c <= 90)
+    return c - 65;
+  else if (97 <= c && c <= 122)
+    return (c - 97) + 26;
+  else if (48 <= c && c <= 57)
+    return (c - 48) + 52;
+  else if (c == '+')
+    return 62;
+  else if (c == '/')
+    return 63;
+
+  /* Should never happen */
+  else
+    return 0;
 }
 
 ICACHE_FLASH_ATTR uint8_t encmap(uint8_t b) {
--- crypto/base64.h
+++ crypto/base64.h
@@ -3,6 +3,7 @@
 
 #include <c_types.h>
 
+int b64_decode(const uint8_t *enc, size_t elen, uint8_t *dec, size_t dlen);
 int b64_encode(const uint8_t *dec, size_t dlen, uint8_t *enc, size_t elen);
 
 #endif
