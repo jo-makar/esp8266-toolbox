f8c1a686cfa1d7deb2de74e5c9e472dd920cdeb5
J. O. Makar <jomakar@gmail.com>
2017-09-06 03:48:38 -0400

    Removed crypto

--- config.h
+++ config.h
@@ -1,19 +1,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include "log/log.h"
-
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
 
-#define str(s) _str(s)
-#define _str(s) #s
-#define VERSION str(VERSION_MAJOR) "." str(VERSION_MINOR) "." str(VERSION_PATCH)
+#define _STR(s) __STR(s)
+#define __STR(s) #s
+#define VERSION _STR(VERSION_MAJOR) "." _STR(VERSION_MINOR) "." _STR(VERSION_PATCH)
 
 #define BUILD_DATETIME __DATE__ " " __TIME__
-
-#define WIFI_SSID_PREFIX "esp8266"
-#define WIFI_PASSWORD    "l0cK*It_Dwn"
 
 #endif
--- crypto/base64.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRYPTO_BASE64_H
-#define CRYPTO_BASE64_H
-
-#include <c_types.h>
-
-int b64_decode(const uint8_t *enc, size_t elen, uint8_t *dec, size_t dlen);
-int b64_encode(const uint8_t *dec, size_t dlen, uint8_t *enc, size_t elen);
-
-#endif
